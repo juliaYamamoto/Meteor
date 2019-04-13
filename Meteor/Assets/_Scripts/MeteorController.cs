e9f8ba0da922462004ef7b9e790c13e79de7818a
juliaYamamoto <juliaYamamoto@users.noreply.github.com>
2019-04-13 00:53:28 -0300

    Project folder

---
+++ Meteor/Assets/_Scripts/MeteorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorController : MonoBehaviour
+{
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("oi"); 
+    }
+
+
+}
